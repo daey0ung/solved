75de0c51b6c55c8a1a06fe8530c55b4cc0f14222
daey0ung <radio010719@gmail.com>
2026-02-17 11:42:58 +0900

    [Bronze I] Title: 최소공배수, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
+    g = gcd(A, B)
+    print(A * B // g)
