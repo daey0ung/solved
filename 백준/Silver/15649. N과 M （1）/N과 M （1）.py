0cd096d970a63658d7af56283e7cc9597a4ebb6c
daey0ung <radio010719@gmail.com>
2026-03-02 09:25:54 +0900

    [Silver III] Title: N과 M (1), Time: 184 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+array = [i for i in range(1,N+1)]
+visited = [False for _ in range(len(array))]
+
+def backtrack(arr):
+	if len(arr) == M:
+		print(*arr, sep=' ',)
+		return
+	
+	for i in range(len(array)):
+		if visited[i] == False:
+			visited[i] = True
+			backtrack(arr + [array[i]])
+			visited[i] = False
+
+backtrack([])
